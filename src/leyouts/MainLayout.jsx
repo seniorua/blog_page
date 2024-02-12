f93b8c066ee38ce8e0c1d1b45a3e2a98f11fb2c2
seniorua <napishy1@.gmail.com>
2024-02-12 20:07:48 +0100

    mainleyout

---
+++ src/leyouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import { Header } from "../conponents/Header";
+import { Footer } from "../conponents/Footer";
+import { Outlet } from "react-router-dom";
+
+export const MainLayout = () => {
+  return (
+    <>
+      <header>
+        <div className=""><Header></Header></div>
+      </header>
+      <main className="">{<Outlet />}</main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </>
+  );
+};
