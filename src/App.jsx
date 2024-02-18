ff71c06685745286ff165d225d79a6c6a7f3b3e7
seniorua <napishy1@gmail.com>
2024-02-18 02:29:09 +0100

    parameters

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,25 @@ import { NotFoundPage } from "./pages/NotFoundPage";
 import { MainLayout } from "./leyouts/MainLayout";
 import { LoginLayout } from "./leyouts/LoginLayout";
 import { Login } from "./pages/Login";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.slingacademy.com/v1/sample-data/blog-posts")
+      .then((data) => data.json())
+      .then((data) => {
+        setArticles(data.blogs);
+      });
+    }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/contacts" element={<Contacts />} />
-          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles" element={<Articles articles={ articles }/>} />
           <Route path="/articles/:id" element={<OneArticle />} />
         </Route>
         <Route path="/login" element={<LoginLayout />}>
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const Articles = () => {
-  const [articles, setArticles] = useState([]);
+export const Articles = ({ articles }) => {
+  // const [articles, setArticles] = useState([]);
   const navigate = useNavigate();
 
   const articleLinkClick = (id) => {
     navigate(`/articles/${id}`, {state: {title: articles[id-1].title, content_text: articles[id-1].content_text}});
   }
 
-  useEffect(() => {
-    fetch("https://api.slingacademy.com/v1/sample-data/blog-posts")
-      .then((data) => data.json())
-      .then((data) => {
-        setArticles(data.blogs);
-      });
-    }, []);
+  // useEffect(() => {
+  //   fetch("https://api.slingacademy.com/v1/sample-data/blog-posts")
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       setArticles(data.blogs);
+  //     });
+  //   }, []);
     
   return (
     <div className="article-container wrapper">
       <h2>List of articles:</h2>
       <div className="article">
-        {articles.map((article) => {
+        {articles?.map((article) => {
           return (
             <div className="article-card" key={article.id}>
               <div>
