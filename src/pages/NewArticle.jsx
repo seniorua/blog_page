bda6b5ec3fe2b34ca2d54fedf6cf878ca9174e27
seniorua <napishy1@gmail.com>
2024-02-19 13:35:58 +0100

    mac bag - to windows

--- src/pages/NewArticle.jsx
+++ src/pages/NewArticle.jsx
@@ -1,24 +1,40 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
 export const NewArticle = () => {
   const navigate = useNavigate();
-  const { state }  = useLocation();
-
+  const title = useRef(undefined);
+  const content = useRef(undefined);
+  
   const toArticles = () => {
     navigate("/");
   };
-
+  
+  const creareArticlePress = () => {
+    console.log('title: ', title);
+    console.log('content: ', content);
+  }
   return (
-    <div className="new-article wrapper">
-      {/* <h3 onClick={toArticles} style={{ cursor: "pointer", color: "blue" }}>
-        {"<"} сделать, чтоб возвращалась на шаг назад
-        {"< "}Home
-      </h3> */}
-      <h1>New Article</h1>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Category" />
-      <textarea name="content" id="" cols="30" rows="10" placeholder="Content of article."></textarea>
-      <textarea name="description" id="" cols="30" rows="10" placeholder="Describe article."></textarea>
+    <div className="wrapper">
+      <h1>New Article:</h1><br />
+      <div className="new-article">
+        {/* <h3 onClick={toArticles} style={{ cursor: "pointer", color: "blue" }}>
+          {"<"} сделать, чтоб возвращалась на шаг назад
+          {"< "}Home
+        </h3> */}
+        <input ref={title} type="text" placeholder="Title" />
+        <input type="text" placeholder="Category" />
+        <input type="text" placeholder="Describtion of article" />
+        <textarea
+          ref={content}
+          name="content"
+          id=""
+          cols="30"
+          rows="10"
+          placeholder="Content of article"
+        ></textarea>
+        <button className="create-article-button" onClick={creareArticlePress}>Create the article</button>
+      </div>
     </div>
   );
 };
