0f42828b30e5e7217fb54bc688d878b537bbda9a
seniorua <napishy1@gmail.com>
2024-02-19 01:57:39 +0100

    reload

--- src/pages/NewArticle.jsx
+++ src/pages/NewArticle.jsx
@@ -10,9 +10,15 @@ export const NewArticle = () => {
 
   return (
     <div className="new-article wrapper">
-      <h4 onClick={toArticles} style={{ cursor: "pointer", color: "blue" }}>
-        {/* {"<"} сделать, чтоб возвращалась на шаг назад */}
-      </h4>
+      {/* <h3 onClick={toArticles} style={{ cursor: "pointer", color: "blue" }}>
+        {"<"} сделать, чтоб возвращалась на шаг назад
+        {"< "}Home
+      </h3> */}
+      <h1>New Article</h1>
+      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Category" />
+      <textarea name="content" id="" cols="30" rows="10" placeholder="Content of article."></textarea>
+      <textarea name="description" id="" cols="30" rows="10" placeholder="Describe article."></textarea>
     </div>
   );
 };
